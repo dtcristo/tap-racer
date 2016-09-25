fa1e6e63f4a9ba652d750c0c63a1d1f5d5576339
David Cristofaro <david@dtcristo.com>
2016-09-25 14:40:17 +1000

    Tweak formatting

--- web/channels/play_channel.ex
+++ web/channels/play_channel.ex
@@ -3,19 +3,22 @@ defmodule TapRacer.PlayChannel do
 
   def join("play", %{"name" => name}, socket) do
     user_id = UUID.uuid4()
-    TapRacer.Endpoint.broadcast! "console", "join",
-                                 %{name: name, user_id: user_id}
+    TapRacer.Endpoint.broadcast(
+      "console", "join", %{name: name, user_id: user_id}
+    )
     {:ok, assign(socket, :user_id, user_id)}
   end
 
   def terminate(_reason, socket) do
-    TapRacer.Endpoint.broadcast! "console", "terminate",
-                                 %{user_id: socket.assigns.user_id}
+    TapRacer.Endpoint.broadcast(
+      "console", "terminate", %{user_id: socket.assigns.user_id}
+    )
   end
 
   def handle_in("tap", _payload, socket) do
-    TapRacer.Endpoint.broadcast! "console", "tap",
-                                 %{user_id: socket.assigns.user_id}
+    TapRacer.Endpoint.broadcast(
+      "console", "tap", %{user_id: socket.assigns.user_id}
+    )
     {:noreply, socket}
   end
 end
