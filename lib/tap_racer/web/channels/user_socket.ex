ddb58862bb607c0f55d3f26ceab063cd1d31252b
David Cristofaro <david@dtcristo.com>
2016-10-16 17:46:35 +1100

    Update comments and add example code

--- lib/tap_racer/web/channels/user_socket.ex
+++ lib/tap_racer/web/channels/user_socket.ex
@@ -4,35 +4,32 @@ defmodule TapRacer.UserSocket do
   ## Channels
   channel "console", TapRacer.ConsoleChannel
   channel "play", TapRacer.PlayChannel
+  # channel "chat:*", TapRacer.ChatChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
   # transport :longpoll, Phoenix.Transports.LongPoll
 
-  # Socket params are passed from the client and can
-  # be used to verify and authenticate a user. After
-  # verification, you can put default assigns into
-  # the socket that will be set for all channels, ie
-  #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
-  #
-  # To deny connection, return `:error`.
-  #
-  # See `Phoenix.Token` documentation for examples in
-  # performing token verification on connect.
+  # Un-authenticated socket
   def connect(_params, socket) do
     {:ok, socket}
   end
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
-  #
-  #     def id(socket), do: "users_socket:#{socket.assigns.user_id}"
-  #
-  # Would allow you to broadcast a "disconnect" event and terminate
-  # all active sockets and channels for a given user:
-  #
-  #     TapRacer.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
-  #
-  # Returning `nil` makes this socket anonymous.
+  # def connect(%{token: token}, socket) do
+  #   case find_user(token) do
+  #     {:ok, user} ->
+  #       {:ok, assign(socket, :user, user)}
+  #     _other ->
+  #       :error
+  #   end
+  # end
+  # def connect(_other, _socket) do: :error
+
+  # Anonymous socket
   def id(_socket), do: nil
+
+  # def id(socket), do: "user_socket:#{socket.assigns.user.id}"
+  #
+  # # Use to diconnect all sockets for user '123'
+  # TapRacer.Endpoint.broadcast("user_socket:123", "disconnect", %{})
 end
