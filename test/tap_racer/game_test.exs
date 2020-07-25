9db7b59b906783bba26ed745e86294966900fa0a
David Cristofaro <david@dtcristo.com>
2020-07-25 13:18:01 +1000

    Change test player ids

--- test/tap_racer/game_test.exs
+++ test/tap_racer/game_test.exs
@@ -11,22 +11,22 @@ defmodule TapRacer.GameTest do
   end
 
   test "join/2 successfully adds player to player map", %{game: game} do
-    assert TapRacer.Game.join(game, "11111111") == :ok
+    assert TapRacer.Game.join(game, "aaaaaaaa") == :ok
 
     assert GenServer.call(game, :state)
            |> Map.fetch!(:players)
-           |> MapSet.member?("11111111")
+           |> MapSet.member?("aaaaaaaa")
   end
 
   test "join/2 fails with a player with duplicate id already joined", %{game: game} do
-    assert TapRacer.Game.join(game, "11111111") == :ok
-    assert TapRacer.Game.join(game, "11111111") == :error
+    assert TapRacer.Game.join(game, "aaaaaaaa") == :ok
+    assert TapRacer.Game.join(game, "aaaaaaaa") == :error
   end
 
   test "notify/2 makes the first time caller the winner", %{game: game} do
-    TapRacer.Game.join(game, "11111111")
-    assert TapRacer.Game.notify(game, "11111111") == :win
-    assert GenServer.call(game, :state) |> Map.fetch!(:winner) == "11111111"
+    TapRacer.Game.join(game, "aaaaaaaa")
+    assert TapRacer.Game.notify(game, "aaaaaaaa") == :win
+    assert GenServer.call(game, :state) |> Map.fetch!(:winner) == "aaaaaaaa"
   end
 
   test "notify/2 fails if player hasn't joined", %{game: game} do
@@ -34,10 +34,10 @@ defmodule TapRacer.GameTest do
   end
 
   test "notify/2 makes the second time caller the loser", %{game: game} do
-    TapRacer.Game.join(game, "11111111")
-    TapRacer.Game.join(game, "22222222")
-    TapRacer.Game.notify(game, "11111111")
-    assert TapRacer.Game.notify(game, "22222222") == :lose
-    assert GenServer.call(game, :state) |> Map.fetch!(:winner) == "11111111"
+    TapRacer.Game.join(game, "aaaaaaaa")
+    TapRacer.Game.join(game, "bbbbbbbb")
+    TapRacer.Game.notify(game, "aaaaaaaa")
+    assert TapRacer.Game.notify(game, "bbbbbbbb") == :lose
+    assert GenServer.call(game, :state) |> Map.fetch!(:winner) == "aaaaaaaa"
   end
 end
