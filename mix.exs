c5051359a842c39244ee991c95f62f84c9590e1a
David Cristofaro <david@dtcristo.com>
2020-07-25 12:07:29 +1000

    Remove uuid dep

--- mix.exs
+++ mix.exs
@@ -46,8 +46,7 @@ defmodule TapRacer.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:uuid, "~> 1.1"}
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 
